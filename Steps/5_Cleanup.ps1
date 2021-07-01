14b909a8978fd15f21efabffc9bc1914193922d2
tomgrice <tomgrice@gmail.com>
2021-07-01 18:02:21 +0100

    Update 5_Cleanup.ps1

--- Steps/5_Cleanup.ps1
+++ Steps/5_Cleanup.ps1
@@ -8,10 +8,10 @@ Write-Host ""
 Write-Host "Performing cleanup of unnecessary install files." -ForegroundColor Yellow
 Remove-Item "$PSScriptRoot\..\Bin\Drivers" -Recurse -Force
 Remove-Item "$PSScriptRoot\..\Bin\vbcable" -Recurse -Force
-Remove-Item "$PSScriptRoot\..\Drivers.zip" -Force
-Remove-Item "$PSScriptRoot\..\redist_x64.exe" -Force
-Remove-Item "$PSScriptRoot\..\redist_x86.exe" -Force
-Remove-Item "$PSScriptRoot\..\Sunshine-Windows.zip" -Force
-Remove-Item "$PSScriptRoot\..\vbcable.zip" -Force
-Remove-Item "$PSScriptRoot\..\ViGEmBus.exe" -Force
+Remove-Item "$PSScriptRoot\..\Bin\Drivers.zip" -Force
+Remove-Item "$PSScriptRoot\..\Bin\redist_x64.exe" -Force
+Remove-Item "$PSScriptRoot\..\Bin\redist_x86.exe" -Force
+Remove-Item "$PSScriptRoot\..\Bin\Sunshine-Windows.zip" -Force
+Remove-Item "$PSScriptRoot\..\Bin\vbcable.zip" -Force
+Remove-Item "$PSScriptRoot\..\Bin\ViGEmBus.exe" -Force
 Write-Host "Clean up complete!" -ForegroundColor Yellow
